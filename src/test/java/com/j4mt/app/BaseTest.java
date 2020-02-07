2694888d32d7deb706530272f7f53b17fd128d28
j4mt <johm.orourke.1985@gmail.com>
2020-02-07 11:06:21 +0000

    adding tests

--- src/test/java/com/j4mt/app/BaseTest.java
+++ src/test/java/com/j4mt/app/BaseTest.java
@@ -6,20 +6,21 @@ import org.junit.Before;
 
 public class BaseTest {
 
-    protected CarResult carResult1, carResult2, carResult3, carResult4, carResult5, carResult6, carResult7, carResult8, carResult9, carResult10;
+    protected CarResult carResultNC1, carResultNC2, carResultNC3, carResult4, carResult5, carResult6, carResult7, carResult8, carResult9, carResultNC10;
 
     @Before
     public void setUpCarSet() {
 
-        carResult1 = new CarResult("Volkswagen Polo", "NIZA", "EDMR", 12.81d, FuelPolicy.FULLEMPTY);
-        carResult2 = new CarResult("Peugeot 107", "CENTAURO", "MCMR", 9.78d, FuelPolicy.FULLEMPTY);
-        carResult3 = new CarResult("Toyota Avensis", "GOLDCAR", "CWMR", 301.95d, FuelPolicy.FULLEMPTY);
+        //    Note corporate cars are those supplied by AVIS, BUDGET, ENTERPRISE, FIREFLY, HERTZ, SIXT, THRIFTY.
+        carResultNC1 = new CarResult("Volkswagen Polo", "NIZA", "EDMR", 12.81d, FuelPolicy.FULLEMPTY);
+        carResultNC2 = new CarResult("Peugeot 107", "CENTAURO", "MCMR", 9.78d, FuelPolicy.FULLEMPTY);
+        carResultNC3 = new CarResult("Toyota Avensis", "GOLDCAR", "CWMR", 301.95d, FuelPolicy.FULLEMPTY);
         carResult4 = new CarResult("Toyota Avensis", "AVIS", "IDMR", 373.69d, FuelPolicy.FULLFULL);
         carResult5 = new CarResult("Peugeot 107", "HERTZ", "MCMR", 67.98d, FuelPolicy.FULLFULL);
         carResult6 = new CarResult("Volkswagen Polo", "FIREFLY", "EDMR", 29.79d, FuelPolicy.FULLEMPTY);
         carResult7 = new CarResult("Ford Focus Estate", "AVIS", "EDMR", 291.28d, FuelPolicy.FULLFULL);
         carResult8 = new CarResult("Ford Focus Estate", "AVIS", "EDMR", 281.28d, FuelPolicy.FULLFULL);
         carResult9 = new CarResult("Citroen Berlingo", "ENTERPRISE", "CMMV", 212.23d, FuelPolicy.FULLFULL);
-        carResult10 = new CarResult("Toyota Avensis", "GOLDCAR", "IDMR", 301.95d, FuelPolicy.FULLEMPTY);
+        carResultNC10 = new CarResult("Toyota Avensis", "GOLDCAR", "IDMR", 301.95d, FuelPolicy.FULLEMPTY);
     }
 }
--- src/test/java/com/j4mt/app/component/CarResultProcessorTest.java
+++ src/test/java/com/j4mt/app/component/CarResultProcessorTest.java
@@ -10,15 +10,41 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 
 public class CarResultProcessorTest extends BaseTest {
 
     @Test
     public void test_CarResultProcessor_check_ordering(){
         CarResultProcessor carResultProcessor = new CarResultProcessorImpl();
-        List<CarResult> list = carResultProcessor.processCars(Arrays.asList(carResult1, carResult6));
+        List<CarResult> list = carResultProcessor.processCars(Arrays.asList(carResultNC1, carResult6));
 
         assertThat(list.get(0), is(carResult6));
-        assertThat(list.get(1), is(carResult1));
+        assertThat(list.get(1), is(carResultNC1));
+    }
+
+    @Test
+    public void test_CarResultProcessor_check_duplicate_not_added(){
+        CarResultProcessor carResultProcessor = new CarResultProcessorImpl();
+        List<CarResult> list = carResultProcessor.processCars(Arrays.asList(carResult7, carResult8));
+
+        assertThat(list.size(), is(1));
+
+    }
+
+    @Test
+    public void test_CarResultProcessor_check_medion_FullFull_not_added(){
+        CarResultProcessor carResultProcessor = new CarResultProcessorImpl();
+        List<CarResult> list = carResultProcessor.processCars(Arrays.asList(carResult6, carResult7));
+
+        assertThat(list.size(), is(1));
+    }
+
+    @Test
+    public void test_CarResultProcessor_check_odering_by_type(){
+        CarResultProcessor carResultProcessor = new CarResultProcessorImpl();
+        List<CarResult> list = carResultProcessor.processCars(Arrays.asList(carResultNC1, carResultNC2, carResultNC3, carResult4, carResult5, carResult6, carResult7, carResult8, carResult9, carResultNC10));
+
+        assertThat(list.size(), is(8));
     }
 }
