e3596e717b19c7e5449828380b1bbf229f8d0933
j4mt <johm.orourke.1985@gmail.com>
2020-02-07 11:57:18 +0000

    updating tests

--- src/test/java/com/j4mt/app/component/CarResultProcessorTest.java
+++ src/test/java/com/j4mt/app/component/CarResultProcessorTest.java
@@ -10,12 +10,11 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
 
 public class CarResultProcessorTest extends BaseTest {
 
     @Test
-    public void test_CarResultProcessor_check_ordering(){
+    public void test_CarResultProcessor_check_ordering() {
         CarResultProcessor carResultProcessor = new CarResultProcessorImpl();
         List<CarResult> list = carResultProcessor.processCars(Arrays.asList(carResultNC1, carResult6));
 
@@ -24,27 +23,18 @@ public class CarResultProcessorTest extends BaseTest {
     }
 
     @Test
-    public void test_CarResultProcessor_check_duplicate_not_added(){
+    public void test_CarResultProcessor_check_duplicate_not_added() {
         CarResultProcessor carResultProcessor = new CarResultProcessorImpl();
         List<CarResult> list = carResultProcessor.processCars(Arrays.asList(carResult7, carResult8));
 
         assertThat(list.size(), is(1));
-
     }
 
     @Test
-    public void test_CarResultProcessor_check_medion_FullFull_not_added(){
+    public void test_CarResultProcessor_check_medion_FullFull_not_added() {
         CarResultProcessor carResultProcessor = new CarResultProcessorImpl();
         List<CarResult> list = carResultProcessor.processCars(Arrays.asList(carResult6, carResult7));
 
         assertThat(list.size(), is(1));
-    }
-
-    @Test
-    public void test_CarResultProcessor_check_odering_by_type(){
-        CarResultProcessor carResultProcessor = new CarResultProcessorImpl();
-        List<CarResult> list = carResultProcessor.processCars(Arrays.asList(carResultNC1, carResultNC2, carResultNC3, carResult4, carResult5, carResult6, carResult7, carResult8, carResult9, carResultNC10));
-
-        assertThat(list.size(), is(8));
     }
 }
